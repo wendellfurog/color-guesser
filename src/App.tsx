dcae22bf1367f23b7b6e117d25200ed13ee026a1
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-10-06 13:16:26 +0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  enum Result {
+    Correct,
+    Wrong
+  }
+
+  const [color, setColor] = useState<string>('');
+  const [answers, setAnswers] = useState<string[]>([]);
+  const [result, setresult] = useState<Result | undefined>(undefined);
+
+
+  const pickColor = () => {
+    // Generate a random color
+    const actualColor = getRandomColor();
+    setColor(actualColor);
+    setAnswers([actualColor, getRandomColor(), getRandomColor()].sort(
+      () => 0.5 - Math.random()
+      )
+    );
+  };
+
+  const getRandomColor = () => {
+    const colorNumber = Math.floor(Math.random()*16777215).toString(16);
+    const colorCode = "#" + colorNumber;
+    return colorCode;
+  };
+
+  useEffect(() => {
+    pickColor();
+  }, []);
+
+  function handleAnswer(answer: string) {
+    if (answer === color) {
+      // guessed correct answer
+      setresult(Result.Correct);
+      // generate new color
+      pickColor();
+    } else {
+      // guessed wrong answer
+      setresult(Result.Wrong);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='column'>
+        <h1 className='title'>Guess the Color</h1>
+        <div className='guess-box' style={{ background: color }}>
+        </div>
+        {answers.map(answer => (
+          <button key={answer} onClick={() => handleAnswer(answer)}>{answer}</button>
+        ))
+        }
+        {result === Result.Wrong && <div className='wrong'>Wrong Answer</div>}
+        {result === Result.Correct && <div className='correct'>Correct Answer!</div>}
+      </div>
     </div>
   );
 }
